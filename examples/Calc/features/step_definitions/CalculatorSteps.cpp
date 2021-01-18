5d7c644d02ece5ac3cb9eedef891435dc7357746
Nathaniel Brough <nathaniel.brough@gmail.com>
2021-01-18 10:21:32 +0800

    build: Adding  bazel build support

--- examples/Calc/features/step_definitions/CalculatorSteps.cpp
+++ examples/Calc/features/step_definitions/CalculatorSteps.cpp
@@ -1,36 +1,37 @@
-#include <cucumber-cpp/autodetect.hpp>
-
 #include <Calculator.h>
+#include <gtest/gtest.h>
+
+#include <cucumber-cpp/autodetect.hpp>
 
 using cucumber::ScenarioScope;
 
 struct CalcCtx {
-    Calculator calc;
-    double result;
+  Calculator calc;
+  double result;
 };
 
 GIVEN("^I have entered (\\d+) into the calculator$") {
-    REGEX_PARAM(double, n);
-    ScenarioScope<CalcCtx> context;
+  REGEX_PARAM(double, n);
+  ScenarioScope<CalcCtx> context;
 
-    context->calc.push(n);
+  context->calc.push(n);
 }
 
 WHEN("^I press add") {
-    ScenarioScope<CalcCtx> context;
+  ScenarioScope<CalcCtx> context;
 
-    context->result = context->calc.add();
+  context->result = context->calc.add();
 }
 
 WHEN("^I press divide") {
-    ScenarioScope<CalcCtx> context;
+  ScenarioScope<CalcCtx> context;
 
-    context->result = context->calc.divide();
+  context->result = context->calc.divide();
 }
 
 THEN("^the result should be (.*) on the screen$") {
-    REGEX_PARAM(double, expected);
-    ScenarioScope<CalcCtx> context;
+  REGEX_PARAM(double, expected);
+  ScenarioScope<CalcCtx> context;
 
-    EXPECT_EQ(expected, context->result);
+  EXPECT_EQ(expected, context->result);
 }
